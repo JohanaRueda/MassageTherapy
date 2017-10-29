582c4c2e23d07c95afc0d1991de1823f2e12fda7
Samantha Ray <sjray32@gmail.com>
2017-10-29 16:28:56 +0000

    Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :users
  #root 'application#hello'
  get "/tabs/:tab" => "tabs#show"
+
+ get  '/test',    to: 'static_pages#home'
+ get  '/help',    to: 'static_pages#help'
+ get  '/about',   to: 'static_pages#about'
+ get  '/contact', to: 'static_pages#contact'
  root "tabs#show", tab: "test"
 end
