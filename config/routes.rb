d2d4c08d11e0a81bc70012cac6c674a60ab385da
Samantha Ray <sjray32@gmail.com>
2017-10-31 20:20:06 +0000

    Changed style for input

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
  get  '/help',    to: 'static_pages#help'
  get  '/about',   to: 'static_pages#about'
  get  '/contact', to: 'static_pages#contact'
+ get  '/course1', to: 'tabs#course1#show'
  root "tabs#show", tab: "test"
 end
