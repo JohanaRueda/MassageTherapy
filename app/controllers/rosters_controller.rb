2aa709b3c34b9c3ea13513c356d06320ba63260b
Jake Leland <jake.leland@att.net>
2017-11-03 16:44:21 -0500

    Created Rosters - Renamed "Roster" -> "Rosters" to make rails happy - Defined index, new, create, show, and destroy actions - Created views for index, new, and show

---
+++ app/controllers/rosters_controller.rb
@@ -0,0 +1,32 @@
+class RostersController < ApplicationController
+  def roster_params
+    params.require(:roster).permit(:user_id, :offering_id)
+  end
+
+  def index
+    @rosters = Roster.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @roster = Roster.new(roster_params)
+    if @roster.save
+      flash[:success] = "Registered user " + @roster.user_id.to_s + " for course offering " + @roster.offering_id.to_s
+      redirect_to @roster
+    end
+  end
+
+  def show
+    @roster = Roster.find(params[:id])
+  end
+
+  def destroy
+    @roster = Roster.find(params[:id])
+    @roster.destroy
+    flash[:notice] = "Roster '#{@roster.id}' deleted."
+    redirect_to rosters_path
+  end
+end
