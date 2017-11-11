fea731cd92d598ae085cb90fa28b1d58967e3898
Samantha Ray <sjray32@gmail.com>
2017-11-11 21:04:43 +0000

    Fixing migration error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :users
+  resources :rosters
  #root 'application#hello'
  get "/tabs/:tab" => "tabs#show"
 
@@ -8,15 +8,12 @@ Rails.application.routes.draw do
  get  '/courses',    to: 'static_pages#courses'
  get  '/contact', to: 'static_pages#contact'
  get  '/login',   to: 'static_pages#login'
- root "tabs#show", tab: "test"
-=======
-   
-  resources :users
-  resources :rosters
-  #get 'users/new'
-
+ 
+ #get 'users/new'
  #root 'application#hello'
  #get "/tabs/:tab" => "tabs#show"
  #root "tabs#show", tab: "test"
->>>>>>> 2aa709b3c34b9c3ea13513c356d06320ba63260b
+ 
+ root "tabs#show", tab: "test"
+
 end
