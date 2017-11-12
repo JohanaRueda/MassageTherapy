7f5d31ab74d1e5d393801cf16a70cb73652ef638
Ellen Stanfill <ellen@ovillavet.com>
2017-11-12 18:07:04 +0000

    controller for courses plus views for index, new, and show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,32 @@
 class CoursesController < ApplicationController
+    def course_params
+        params.require(:course).permit(:courseName, :courseDesc)
+    end
+    
+    def index
+        @courses = Course.all
+    end
+    
+    def new
+        # default: render 'new' template
+    end
+    
+    def create
+        @course = Course.new(course_params)
+        if @course.save
+            flash[:success] = "Created new course " + @course.courseName.to_s
+            redirect_to @course
+        end
+    end
+    
+    def show
+        @course = Course.find(params[:id])
+    end
+    
+    def destroy
+        @course = Course.find(params[:id])
+        @course.destroy
+        flash[:notice] = "Course '#{@course.id}' deleted."
+        redirect_to courses_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
    
   resources :users
+  resources :courses
   #get 'users/new'
 
  #root 'application#hello'
