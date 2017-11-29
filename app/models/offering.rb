3fb4a797dc386fd9979980e06671a07fa48d518a
Jake Leland <jake.leland@att.net>
2017-11-29 17:52:16 -0600

    Set up database relations properly so that associations are destroyed - Deleting a course will delete associated offerings - Deleting an offering will delete associated rosters - Deleting a user will delete associated rosters

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -1,2 +1,5 @@
 class Offering < ApplicationRecord
+  belongs_to :course
+  has_many :rosters, dependent: :destroy
+  has_many :users, through: :rosters
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,30 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+  has_many :rosters, dependent: :destroy
+  has_many :offerings, through: :rosters
 
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
-    
-    VALID_PHONE_REGEX= /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
-    validates :phone, presence: true, length: { minimum: 10 }, format: { with: VALID_PHONE_REGEX }
-    
-    validates :address, presence: true
-    # validates :admin, presence: true
-    validates :license, length: { minimum: 8 }
-    
-    def self.to_csv
-        attributes= %w{ id name email password admin}
-        CSV.generate(headers: true) do |csv|
-            csv << attributes
-            all.each do |user|
-                csv << user.attributes.values_at(*attributes)
-            end
-        end
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
+  VALID_PHONE_REGEX= /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+  validates :phone, presence: true, length: {minimum: 10}, format: {with: VALID_PHONE_REGEX}
+
+  validates :address, presence: true
+  # validates :admin, presence: true
+  validates :license, length: {minimum: 8}
+
+  def self.to_csv
+    attributes= %w{ id name email password admin}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |user|
+        csv << user.attributes.values_at(*attributes)
+      end
     end
+  end
 
 end
