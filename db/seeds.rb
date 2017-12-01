606cf0db50c05393dc3e001a96ff4c73672e81da
Samantha Ray <sjray32@gmail.com>
2017-12-01 18:46:12 +0000

    merging with emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "John Smith",
+            email: "john.smith@gmail.com",
+            password:              "password",
+            password_confirmation: "password",
+            address: "foobar",
+            phone: "8880001111",
+            license: "12345678",
+            admin:     true)
+
+User.create!(name:  "Jane Smith",
+            email: "jane.smith@gmail.com",
+            password:              "password",
+            password_confirmation: "password",
+            address: "foobar",
+            phone: "8880002222",
+            license: "12341234",
+            admin:     false)
+
+#User.create!(name:  "Example User",
+#             email: "example@railstutorial.org",
+#             password:              "foobar",
+#             password_confirmation: "foobar",
+#             address: "foobar",
+#             phone: "8880001111",
+#             license: "12345678",
+#             admin:     true)
+             
+#99.times do |n|
+#  name  = "#{n} Bar"
+#  email = "example-#{n+1}@railstutorial.org"
+#  password = "password"
+#  address = "Texas"
+#  phone = "8880001111"
+#  license = "12345678#{n}"
+#  User.create!(name:  name,
+#              email: email,
+#              address: address,
+#              phone: phone,
+#              license: license,
+#              password:              password,
+#              password_confirmation: password)
+#  end
