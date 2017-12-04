9f4c6b66b81ba7feb1bcb46db148252c10df227d
Ellen Stanfill <ellen@ovillavet.com>
2017-12-04 18:33:18 +0000

    payment succeeds

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,4 @@
 class ChargesController < ApplicationController
-  before_action :amount_to_be_charged
-  before_action :set_description
-
   def thanks
   end
   
@@ -13,26 +10,11 @@ class ChargesController < ApplicationController
                                           stripe_token: params[:stripeToken])
 
     charge = StripeTool.create_charge(customer_id: customer.id, 
-                                      amount: @amount,
-                                      description: @description)
-    # redirect_to rosters_path(roster: {user_id: session[:user_id], offering_id: @o.id})
+                                      amount: params[:amount].to_i,
+                                      description: params[:description])
+    redirect_to rosters_path(roster: {user_id: session[:user_id], offering_id: params[:offering_id]}), method: :post
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
-
-  # def update_amount(amount_in_cents)
-  #     puts amount_in_cents
-  #     @amount = amount_in_cents
-  #     # @O = offering
-  # end
-    
-  private
-    def amount_to_be_charged
-      @amount = 100
-    end
-    
-    def set_description
-      @description = "Registered course"
-    end
 end
--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -1,11 +1,3 @@
 module ChargesHelper
-    # def update_current_offering(offering)
-    #     @o = offering
-    # end
     
-    def update_amount(amount_in_cents, offering)
-        puts amount_in_cents
-        @amount = amount_in_cents
-        @O = offering
-    end
 end
