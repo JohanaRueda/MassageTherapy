c80071253b6e334134f34513e0bedc2b66038cfc
Jake Leland <jake.leland@att.net>
2017-12-04 12:58:44 -0600

    Replaced redirect_to, which wasn't working to save rosters

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,20 @@
 class ChargesController < ApplicationController
-  def thanks
-  end
-  
-  def new
-  end
-
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail], 
+    customer = StripeTool.create_customer(email: params[:stripeEmail],
                                           stripe_token: params[:stripeToken])
 
-    charge = StripeTool.create_charge(customer_id: customer.id, 
+    charge = StripeTool.create_charge(customer_id: customer.id,
                                       amount: params[:amount].to_i,
                                       description: params[:description])
-    redirect_to rosters_path(roster: {user_id: session[:user_id], offering_id: params[:offering_id]}), method: :post
+
+    @roster = Roster.new(user_id: session[:user_id], offering_id: params[:offering_id])
+
+    if @roster.save
+      @offering = Offering.find(@roster.offering_id)
+      flash[:success] = "Registered user " + @roster.user_id.to_s + " for course offering " + @roster.offering_id.to_s
+      redirect_to course_path(@offering.course_id)
+    end
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
