9dfe02981cc0e27af0d99923ae0eee97047a7ed9
Samantha Ray <sjray32@gmail.com>
2017-12-06 17:51:53 +0000

    testing bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :charges, only: [:new, :create]
   #resources :password_resets, only: [:new, :create, :edit, :update]
   resources :password_resets, only: [ :new, :create]
-  get '/password_resets/edit' => 'password_resets#edit', as: :edit_password_reset
+  get '/password_resets/' => 'password_resets#edit', as: :edit_password_reset
   put '/password_resets/' => 'password_resets#update', as: :password_reset
 
   get '/home', to: 'static_pages#home'
